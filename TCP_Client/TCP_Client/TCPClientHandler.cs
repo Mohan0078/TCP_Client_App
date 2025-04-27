6ea87f9d20324d49c64edf0dc9bb45ecefba6403
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-27 16:46:17 +0530

    feat: tcp client handler

---
+++ TCP_Client/TCP_Client/TCPClientHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client
+{
+    public class TCPClientHandler
+    {
+        private const int Port = 3000;
+        private const string ServerAddress = "127.0.0.1"; // Localhost
+    }
+}
