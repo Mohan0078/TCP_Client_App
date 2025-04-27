772125fa74cd3d3c67f33734a9195d0df13ec380
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-27 17:21:58 +0530

    feat: tcp server packet response

---
+++ TCP_Client/TCP_Client/TCPServerPacketResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TCP_Client
+{
+    public class TCPServerPacketResponse
+    {
+        public string Symbol { get; set; }
+        public char BuyOrSell { get; set; }
+        public Int32 Quantity { get; set; }
+        public Int32 Price { get; set; }
+        public Int32 Sequence { get; set; }
+    }
+}
