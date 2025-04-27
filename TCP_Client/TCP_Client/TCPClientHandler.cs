f1e50eb5d1715b1b27fcb6b7cf826dbbaf104b88
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-27 16:53:25 +0530

    feat: instantiate tcp client

--- TCP_Client/TCP_Client/TCPClientHandler.cs
+++ TCP_Client/TCP_Client/TCPClientHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,14 @@ namespace TCP_Client
     {
         private const int Port = 3000;
         private const string ServerAddress = "127.0.0.1"; // Localhost
+        private static TcpClient _tcpClient;
+        private static NetworkStream _networkStream;
+
+        private static void InstantiateTCPClient()
+        {
+            // Connect to the server
+            _tcpClient = new TcpClient(ServerAddress, Port);
+            _networkStream = _tcpClient.GetStream();
+        }
     }
 }
