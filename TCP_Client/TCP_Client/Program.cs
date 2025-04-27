6288ed7eee229cc1723efe134ba6bb662a49aa67
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-04-27 18:56:23 +0530

    feat: calling handle tcp client

--- TCP_Client/TCP_Client/Program.cs
+++ TCP_Client/TCP_Client/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TCP_Client;
+
+// calling the tcp client for handling the tcp request and response
+TCPClientHandler.HandleTCPClient();
